c41bad352ac534cb218307c95fa10963bc8aaa30
Kijowski <wolodymyr.k8@gmail.com>
2025-07-18 03:45:40 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,7 +16,6 @@ locale-gen
 
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "KEYMAP=us" > /etc/vconsole.conf
-
 echo "archpc" > /etc/hostname
 
 # 2. Настройка mkinitcpio для LUKS
@@ -24,8 +23,11 @@ echo "Configuring mkinitcpio hooks..."
 sed -i 's/^HOOKS=.*/HOOKS=(base udev autodetect keyboard keymap modconf block encrypt filesystems fsck)/' /etc/mkinitcpio.conf
 mkinitcpio -P
 
-# 3. Установка и настройка GRUB
-echo "Installing and configuring GRUB..."
+# 3. Установка и настройка GRUB (UEFI)
+echo "Installing GRUB and efibootmgr..."
+pacman -S --noconfirm grub efibootmgr
+
+echo "Configuring GRUB with LUKS UUID..."
 UUID_ROOT=$(blkid -s UUID -o value /dev/nvme0n1p3)
 if [ -z "$UUID_ROOT" ]; then
   echo "ERROR: Could not find UUID for /dev/nvme0n1p3"
@@ -33,12 +35,10 @@ if [ -z "$UUID_ROOT" ]; then
 fi
 
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-
 sed -i "s|^GRUB_CMDLINE_LINUX=.*|GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=$UUID_ROOT:cryptroot root=/dev/mapper/cryptroot\"|" /etc/default/grub
-
 grub-mkconfig -o /boot/grub/grub.cfg
 
-# 4. Создание пользователя и пароль root
+# 4. Пользователь root и wk
 echo "Setting root password..."
 echo "root:1234" | chpasswd
 
@@ -46,32 +46,39 @@ echo "Creating user 'wk' and setting password..."
 useradd -mG wheel wk
 echo "wk:1234" | chpasswd
 
-echo "Configuring sudoers for wheel group..."
+echo "Enabling sudo for wheel group..."
 sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
-# 5. Включение служб NetworkManager и SDDM
-echo "Enabling NetworkManager and SDDM services..."
+# 5. Службы
+echo "Enabling NetworkManager and SDDM..."
+pacman -S --noconfirm networkmanager sddm
 systemctl enable NetworkManager
 systemctl enable sddm
 
-# 6. Установка драйверов Intel
+# 6. Драйвера Intel
 echo "Installing Intel CPU and graphics drivers..."
 pacman -S --noconfirm intel-ucode mesa vulkan-intel xf86-video-intel libva-intel-driver libva-utils
 
 # 7. Установка AUR помощника paru
-echo "Installing paru AUR helper..."
-cd /home/wk
-sudo -u wk git clone https://aur.archlinux.org/paru.git
-chown -R wk:wk paru
+echo "Installing paru (AUR helper)..."
+sudo -u wk bash -c "
+cd ~
+git clone https://aur.archlinux.org/paru.git
 cd paru
-sudo -u wk makepkg -si --noconfirm
+makepkg -si --noconfirm
+"
 
-# 8. Установка Hyprland и зависимостей через paru
-echo "Installing Hyprland and dependencies via paru..."
-sudo -u wk paru -S --noconfirm hyprland waybar rofi dunst kitty thunar polkit-kde-agent pipewire pipewire-pulse wireplumber xdg-desktop-portal-hyprland xdg-desktop-portal wl-clipboard qt5-wayland qt6-wayland nwg-look
+# 8. Установка Hyprland и зависимостей
+echo "Installing Hyprland and required packages..."
+sudo -u wk paru -S --noconfirm \
+  hyprland waybar rofi dunst kitty thunar \
+  polkit-kde-agent pipewire pipewire-pulse \
+  wireplumber xdg-desktop-portal-hyprland \
+  xdg-desktop-portal wl-clipboard qt5-wayland \
+  qt6-wayland nwg-look
 
-# 9. Настройка Hyprland конфигурации
-echo "Setting up Hyprland config..."
+# 9. Настройка Hyprland
+echo "Configuring Hyprland for user wk..."
 sudo -u wk mkdir -p /home/wk/.config/hypr
 sudo -u wk cp /usr/share/hyprland/examples/hyprland.conf /home/wk/.config/hypr/
 
